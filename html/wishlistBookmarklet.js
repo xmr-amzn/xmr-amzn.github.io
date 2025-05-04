89be1deee31d69aecc7ac56e91e10aa51d58d6fe
m3c <m3c@sm.2xwave.com>
2025-05-04 18:09:58 -0400

    Add files via upload

--- html/wishlistBookmarklet.js
+++ html/wishlistBookmarklet.js
@@ -1,10 +1,10 @@
 (function() {
   try {
-    const pageState = Array.from(document.querySelectorAll('ul#g-items'));
+    const items = Array.from(document.querySelectorAll('ul#g-items'));
 //    const trackingId = pageState.trackingId.trim();
 //    const amazonOrderId = location.href.match(/orderId=(\d{3}-\d{7}-\d{7})/)[1];
     let statusText = '';
-console.log(pageState);
+console.log(items);
 /*
 try {
       statusText = pageState.promise.promiseMessage.trim();

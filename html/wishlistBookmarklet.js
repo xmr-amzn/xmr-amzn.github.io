194764b37c24dabaef03b2e050e5fae4aa2f6b25
m3c <m3c@sm.2xwave.com>
2025-05-04 17:48:48 -0400

    Add files via upload

---
+++ html/wishlistBookmarklet.js
@@ -0,0 +1,29 @@
+(function() {
+  try {
+    const pageState = JSON.parse(document.querySelector('ul#g-items').innerHTML);
+//    const trackingId = pageState.trackingId.trim();
+//    const amazonOrderId = location.href.match(/orderId=(\d{3}-\d{7}-\d{7})/)[1];
+    let statusText = '';
+console.log(pageState);
+/*
+try {
+      statusText = pageState.promise.promiseMessage.trim();
+    } catch (err) {
+      ;
+    }
+
+    if (!amazonOrderId) {
+      alert("Cannot retrieve amazon order ID.  are you sure you're on an Amazon tracking page");
+    }
+
+    if (trackingId) {
+      alert("Amazon Order ID: " + amazonOrderId + "\nTracking Number: " + trackingId);
+    } else {
+      alert("It looks like the package hasn't been shipped yet.\nTry again later once it's been shipped");
+    }
+      */
+  } catch (error) {
+    console.log('Wishlist-Bookmarklet Error:', error);
+    alert("Unable to retrieve Wishlist Data.\nAre you sure you're on an Amazon wishlist page?");
+  }
+})();

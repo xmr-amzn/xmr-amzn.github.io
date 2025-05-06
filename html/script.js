8359efb5e09100023154dfc748a5508abb93d86a
m3c <m3c@sm.2xwave.com>
2025-05-06 16:21:08 -0400

    Add files via upload

--- html/script.js
+++ html/script.js
@@ -11,12 +11,19 @@
   });
 
   $("footer").load("index.html footer div.content");
-  $("footer").on("click", ".content a", function(event) { event.preventDefault(); $("footer .content div").toggle(); });
+  $("footer").on("click", ".content a", function(event) { event.preventDefault(); $("footer .content div").toggle(); $("html, body").animate({ scrollTop: $(document).height() }, 250); });
 
+  $("div.qst").click(function() { 
+    $(this).next('div.ans').slideToggle(300);
+    $(this).children('img').toggleClass('flip');
+  });
+
+  $("table.example a").click(function(){ return false; });
+  
   $('a.popup').click(function(event) { 
     event.preventDefault(); 
     $("div#popgray").fadeIn(100);
-    $("div#popup").load("devs.html section#"+$(this).attr('data'), function(response, status, xhr) {
+    $("div#popup").load("devs.html?ts=" + new Date().getTime() + " section#"+$(this).attr('data'), function(response, status, xhr) {
       if(status == "success") {
         $("div#popup").prepend('<div id="popheader">Developer Notes</div>');
         $("div#popup").append('<div id="popfooter"><button>Close</button></div>');
